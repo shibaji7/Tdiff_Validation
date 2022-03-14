15b791e2d89ec95ed330d5d4615df9e6f39bb0dd
Xueling Shi <xueling7@vt.edu>
2022-03-14 10:55:40 -0600

    Calculate virtual height based on r, elv, hop
    Calculate virtual height based on slant range, elevation angle, and hop

--- py/calc_vheight.py
+++ py/calc_vheight.py
@@ -4,5 +4,16 @@
    This file is a v_Height estimator
 """
 
-def calculate_vHeight():
-    return
+def calculate_vHeight(r, elv, hop, Re = 6371.0):
+    """
+    Parameters
+    ----------
+    r: slant range in km
+    elv: elevation angle in degree
+    hop: back scatter hop (0.5,1,1.5, etc.)
+    Re: Earth radius in km
+    """
+   
+    h = np.sqrt(Re**2 + (r/(2*N))**2 + (r/N)*Re*np.sin(np.radians(elv))) - Re
+      
+    return h
